b91febab5f356dc157c35f04b7e9afae47824a52
YernarT <toktaryernar@gmail.com>
2022-11-02 21:40:49 +0600

    update: :hammer: accept className props

--- components/SwitchLanguage/index.jsx
+++ components/SwitchLanguage/index.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 import styles from './style.module.css';
 
-export default function SwitchLanguage() {
+export default function SwitchLanguage({ className }) {
 	const [currentLanguage, setCurrentLanguage] = useState('Рус');
 
 	return (
-		<div className={styles.wrap}>
+		<div className={`${styles.wrap} ${className}`}>
 			<div className={styles.switchLanguage}>
 				<span>{currentLanguage}</span>
 			</div>
