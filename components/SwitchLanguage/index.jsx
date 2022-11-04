bf860bcae9f1d6e5f0a94092b7eda7361bfe4f46
YernarT <toktaryernar@gmail.com>
2022-11-04 20:36:38 +0600

    update: :hammer: 切换语言

--- components/SwitchLanguage/index.jsx
+++ components/SwitchLanguage/index.jsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './style.module.css';
 
+const languages = ['Рус', 'Қаз', 'Нем'];
+
 export default function SwitchLanguage({ className }) {
-	const [currentLanguage, setCurrentLanguage] = useState('Рус');
+	const [currentLanguageIdx, setCurrentLanguageIdx] = useState(0);
+
+	useEffect(() => {
+		let beforeLanguage = localStorage.getItem('language');
+
+		if (beforeLanguage) {
+			setCurrentLanguageIdx(Number(beforeLanguage));
+		}
+	}, []);
+
+	useEffect(() => {
+		const handler = () => {
+			localStorage.setItem('language', currentLanguageIdx);
+		};
+
+		window.addEventListener('beforeunload', handler);
+
+		return () => {
+			window.removeEventListener('beforeunload', handler);
+		};
+	}, [currentLanguageIdx]);
 
 	return (
 		<div className={`${styles.wrap} ${className}`}>
-			<div className={styles.switchLanguage}>
-				<span>{currentLanguage}</span>
+			<div
+				className={styles.switchLanguage}
+				onClick={() => {
+					if (currentLanguageIdx === languages.length - 1) {
+						setCurrentLanguageIdx(0);
+					} else {
+						setCurrentLanguageIdx(prevIdx => prevIdx + 1);
+					}
+				}}>
+				<span>{languages[currentLanguageIdx]}</span>
 			</div>
 		</div>
 	);
