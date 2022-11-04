c5ba32390a160e356b0e08322b208f9bec8351d4
YernarT <toktaryernar@gmail.com>
2022-11-05 04:54:38 +0600

    feat: :sparkles: i18n texts

---
+++ i18n/ger.js
@@ -0,0 +1,50 @@
+const texts = {
+	navbar: {
+		n1: 'Über die Veranstaltung',
+		n2: 'Programm',
+		n3: 'Sprecher',
+		n4: 'Kontakte',
+	},
+
+	global: {
+		register: 'Registrieren',
+	},
+
+	intro: {
+		title: '8 November | Online',
+		desc: `Erweiterte Konferenz zu den Ergebnissen der Umsetzung des Programms <span class="light">„Fit for Partnership with Germany“</span> und des Instruments <span class="light">„Geschäftsverbindungen“</span> im Rahmen des Nationalen Projekts zur Entwicklung des Unternehmertums 2021-2025.`,
+		info1: {
+			date: '1',
+			desc: 'Tag',
+		},
+		info2: {
+			date: '23',
+			desc: 'Lautsprecher',
+		},
+		info3: {
+			date: '23',
+			desc: 'Praxisfälle',
+		},
+		info4: {
+			desc: 'Zugang zu Auslandserfahrung',
+		},
+	},
+
+	info: {
+		title: 'Über die Konferenz',
+		desc: 'Im Rahmen der Konferenz finden Sie interessante Schulungen und Treffen mit deutschen und kasachischen Unternehmen, die dank gutem Management, neuem Denken und der Einführung neuer Technologien wirtschaftliche Erfolge erzielt haben.',
+		subtitle: 'Am Ende der Veranstaltung erhalten Sie:',
+		item1: 'viele nützliche Informationen;',
+		item2: 'Horizontale Verbindungen herstellen;',
+		item3: 'Erhöhen Sie Ihre Kompetenz.',
+	},
+
+	footer: {
+		title: 'Kontaktiere uns',
+
+		key1: 'Kontakt Nummer:',
+		key2: 'E-Mail-Addresse:',
+	},
+};
+
+export default texts;
---
+++ i18n/index.js
@@ -0,0 +1,3 @@
+export { default as textsRU } from './ru';
+export { default as textsKZ } from './kz';
+export { default as textsGER } from './ger';
