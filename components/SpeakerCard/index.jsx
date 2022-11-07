85222c3fb0c7dc28a75433923fa7606805033687
YernarT <toktaryernar@gmail.com>
2022-11-07 10:27:59 +0600

    update: :hammer: update new speakers

--- components/SpeakerCard/index.jsx
+++ components/SpeakerCard/index.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 import styles from './style.module.css';
 
-export default function SpeakerCard({ speaker }) {
+export default function SpeakerCard({ speaker, noImage }) {
 	return (
 		<div className={styles.speakerCard}>
-			<img src={speaker.image} alt={speaker.name} />
+			<div className="imgWrap">
+				{noImage && <img src={speaker.image} alt={speaker.name} />}
+			</div>
 
 			<h4 className="name">{speaker.name}</h4>
 			<p className="info">{speaker.info}</p>
